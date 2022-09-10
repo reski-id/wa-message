482f8ef5386615981d1f593ced4f6914052f0450
reski-id <pesan.reski@gmail.com>
2022-09-10 20:02:20 +0700

    user

--- factory/factory.go
+++ factory/factory.go
@@ -8,11 +8,18 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+
+	ud "wa/feature/user/data"
+	userDelivery "wa/feature/user/delivery"
+	us "wa/feature/user/usecase"
 )
 
 func Initfactory(e *echo.Echo, db *gorm.DB) {
 	//factory
+	userData := ud.New(db)
 	validator := validator.New()
+	useCase := us.New(userData, validator)
+	userDelivery.New(e, useCase)
 
 	msgData := msg.New(db)
 	msgCase := mu.New(msgData, validator)
